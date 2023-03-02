02f103b0db454607a911b130f76705a860fd2642
Ahmede <s2201198@bc.edu.fi>
2023-03-02 09:55:02 +0200

    code first

---
+++ NET22S_CodeFirst/Models/Net22SContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
+
+namespace NET22S_CodeFirst.Models
+{
+    public class Net22SDbContext : DbContext
+    {
+        public Net22SDbContext() : base()
+        {
+
+        }
+
+        public Net22SDbContext(DbContextOptions<Net22SDbContext> opt)
+            : base(opt)
+        {
+            Console.WriteLine("Checking if tables are ready for use");
+
+            if (!TablesReadyForUse())
+            {
+                Console.WriteLine("Creating tables");
+                var relDatcreator = (RelationalDatabaseCreator)this.Database.GetService<IDatabaseCreator>();
+                relDatcreator.CreateTables();
+            }
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connStr = "Data Source=localhost,port;Initial Catalog=idk;User Id=sa;Password=your_password;Encrypt=False;TrustServerCertificate=True";
+            Console.WriteLine("configuring DB context using connectionstring :" + connStr);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+        bool TablesReadyForUse()
+        {
+            try
+            {
+                var studentsTable = this.Students;
+
+                var first = this.Students.FirstOrDefault();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("there was ex: " + ex.Message);
+                return false;
+            }
+        }
+
+
+        //entities
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ NET22S_CodeFirst/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace NET22S_CodeFirst.Models
+{
+	[Table("Students")]
+	public class Student
+	{
+		[Key]
+			public long StudentId { get; set; }
+			public int StudentDetailsId;
+			public string Address;
+			public string PhoneNumber;
+			public string FirstName;
+			public string LastName;
+    }
+}
+
